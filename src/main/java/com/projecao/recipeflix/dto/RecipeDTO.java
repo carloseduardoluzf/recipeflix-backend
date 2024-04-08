2dcf1c267436c5053dcc7add7c7174af01c2b532
carloseduardoluzf <gitdoduds@gmail.com>
2024-04-08 20:30:36 -0300

    separando as pastas

--- src/main/java/com/projecao/recipeflix/dto/RecipeDTO.java
+++
@@ -1,5 +0,0 @@
-package com.projecao.recipeflix.dto;
-
-
-public record RecipeDTO(String title, String description, String image, String instructions, String authorName) {
-}
--- src/main/java/com/projecao/recipeflix/dto/UserRegisterDTO.java
+++
@@ -1,4 +0,0 @@
-package com.projecao.recipeflix.dto;
-
-public record UserRegisterDTO(Long id, String username, String password, String authorName) {}
-
